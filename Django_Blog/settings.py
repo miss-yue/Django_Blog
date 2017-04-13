01b79be482b46e00de7a77eba68880dfab49324c
miss-yue <754442114@qq.com>
2017-04-13 21:56:41 +0800

    设置数据库配置

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 """
 Django settings for Django_Blog project.
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #添加 article 模块
+#   添加 article 模块
     'article',
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Django_Blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
