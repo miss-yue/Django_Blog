f3c4d4b7c1e517ac9fc2585da14d24162e8421b1
miss-yue <754442114@qq.com>
2017-04-14 20:02:22 +0800

    将 Article 注入admin 后台管理

--- article/admin.py
+++ article/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from article.models import Article
 
 # Register your models here.
+admin.site.register(Article) # 将 Article 注入 admin 后台管理
